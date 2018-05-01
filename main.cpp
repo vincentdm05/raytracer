095baa0a55780c7deb4a727786e31cb1a3b716ea
Vincent de Marignac <vincentdm_05@yahoo.fr>
2018-05-01 19:03:30 +0300

    template vec3 by component type

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void printTestImage()
 	{
 		for (int col = 0; col < nx; col++)
 		{
-			vec3 v(float(col) / nx, float(row) / ny, 0.2f);
+			vec3<> v(float(col) / nx, float(row) / ny, 0.2f);
 			v *= 255.99;
 			std::cout << int(v.r) << " " << int(v.g) << " " << int(v.b) << "\n";
 		}
@@ -20,16 +20,16 @@ void printTestImage()
 
 void testVec3()
 {
-	vec3 v(1, 2, 3);
+	vec3<> v(1, 2, 3);
 	std::cout << v << std::endl;
 	std::cout << v.length() << std::endl;
 	std::cout << v.normalize() << std::endl;
 	std::cout << v.length() << std::endl;
 
-	vec3 a;
+	vec3<> a;
 	std::cout << +a << std::endl;
 	std::cout << -a << std::endl;
-	vec3 b(1, 2, 3);
+	vec3<> b(1, 2, 3);
 	std::cout << -b << std::endl;
 	std::cout << a + b << std::endl;
 	std::cout << a - b << std::endl;
@@ -45,9 +45,9 @@ void testVec3()
 	a *= 2;
 	std::cout << a << std::endl;
 	std::cout << dot(a, b) << std::endl;
-	std::cout << cross(vec3(1, 0, 0), vec3(0, 1, 0)) << std::endl;
+	std::cout << cross(vec3<>(1, 0, 0), vec3<>(0, 1, 0)) << std::endl;
 
-	vec3 c(4, 0, 0);
+	vec3<> c(4, 0, 0);
 	std::cout << c << std::endl;
 	std::cout << normalize(c) << std::endl;
 	std::cout << c << std::endl;
--- vec3.h
+++ vec3.h
@@ -4,45 +4,132 @@
 #include <math.h>
 #include <stdlib.h>
 
-typedef float real_t;
-
+template<class comp_t=float>
 class vec3
 {
 private:
-	real_t e[3];
+	comp_t e[3];
 
 public:
-	real_t &x = e[0];
-	real_t &y = e[1];
-	real_t &z = e[2];
-	real_t &r = e[0];
-	real_t &g = e[1];
-	real_t &b = e[2];
+	comp_t &x = e[0];
+	comp_t &y = e[1];
+	comp_t &z = e[2];
+	comp_t &r = e[0];
+	comp_t &g = e[1];
+	comp_t &b = e[2];
 
 	vec3() { x = 0; y = 0; z = 0; }
-	vec3(real_t _x, real_t _y, real_t _z) { x = _x; y = _y; z = _z; }
-	~vec3() {}
+	vec3(comp_t _x, comp_t _y, comp_t _z) { x = _x; y = _y; z = _z; }
 
 	inline const vec3 &operator+() const { return *this; }
 	inline vec3 operator-() const { return vec3(-x, -y, -z); }
-	inline real_t operator[](uint i) const { return e[i]; }
-	inline real_t &operator[](uint i) { return e[i]; }
+	inline comp_t operator[](uint i) const { return e[i]; }
+	inline comp_t &operator[](uint i) { return e[i]; }
 
 	inline vec3 &operator+=(const vec3 &v);
 	inline vec3 &operator-=(const vec3 &v);
 	inline vec3 &operator*=(const vec3 &v);
 	inline vec3 &operator/=(const vec3 &v);
-	inline vec3 &operator+=(real_t c);
-	inline vec3 &operator-=(real_t c);
-	inline vec3 &operator*=(real_t c);
-	inline vec3 &operator/=(real_t c);
+	inline vec3 &operator+=(comp_t c);
+	inline vec3 &operator-=(comp_t c);
+	inline vec3 &operator*=(comp_t c);
+	inline vec3 &operator/=(comp_t c);
 
-	inline real_t squaredLength() const { return x * x + y * y + z * z; }
-	inline real_t length() const { return sqrt(squaredLength()); }
+	inline comp_t squaredLength() const { return x * x + y * y + z * z; }
+	inline comp_t length() const { return sqrt(squaredLength()); }
 	inline vec3 &normalize();
+
+	friend inline std::istream &operator>>(std::istream &is, vec3 &v)
+	{
+		is >> v.x >> v.y >> v.z;
+		return is;
+	}
+
+	friend inline std::ostream &operator<<(std::ostream &os, const vec3 &v)
+	{
+		os << v.x << " " << v.y << " " << v.z;
+		return os;
+	}
+
+	friend inline vec3 operator+(const vec3 &a, const vec3 &b)
+	{
+		return vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+	}
+
+	friend inline vec3 operator-(const vec3 &a, const vec3 &b)
+	{
+		return vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+	}
+
+	friend inline vec3 operator*(const vec3 &a, const vec3 &b)
+	{
+		return vec3(a.x * b.x, a.y * b.y, a.z * b.z);
+	}
+
+	friend inline vec3 operator/(const vec3 &a, const vec3 &b)
+	{
+		return vec3(a.x / b.x, a.y / b.y, a.z / b.z);
+	}
+
+	friend inline vec3 operator+(const vec3 &v, comp_t c)
+	{
+		return vec3(v.x + c, v.y + c, v.z + c);
+	}
+
+	friend inline vec3 operator-(const vec3 &v, comp_t c)
+	{
+		return vec3(v.x - c, v.y - c, v.z - c);
+	}
+
+	friend inline vec3 operator*(const vec3 &v, comp_t c)
+	{
+		return vec3(v.x * c, v.y * c, v.z * c);
+	}
+
+	friend inline vec3 operator/(const vec3 &v, comp_t c)
+	{
+		return vec3(v.x / c, v.y / c, v.z / c);
+	}
+
+	friend inline vec3 operator+(comp_t c, const vec3 &v)
+	{
+		return v + c;
+	}
+
+	friend inline vec3 operator-(comp_t c, const vec3 &v)
+	{
+		return v - c;
+	}
+
+	friend inline vec3 operator*(comp_t c, const vec3 &v)
+	{
+		return v * c;
+	}
+
+	friend inline vec3 operator/(comp_t c, const vec3 &v)
+	{
+		return vec3(c / v.x, c / v.y, c / v.z);
+	}
+
+	friend inline comp_t dot(const vec3 &a, const vec3 &b)
+	{
+		return a.x * b.x + a.y * b.y + a.z * b.z;
+	}
+
+	friend inline vec3 cross(const vec3 &a, const vec3 &b)
+	{
+		return vec3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+	}
+
+	friend inline vec3 normalize(const vec3 &v)
+	{
+		comp_t invl = comp_t(1) / v.length();
+		return v * invl;
+	}
 };
 
-inline vec3 &vec3::operator+=(const vec3 &v)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator+=(const vec3 &v)
 {
 	x += v.x;
 	y += v.y;
@@ -50,7 +137,8 @@ inline vec3 &vec3::operator+=(const vec3 &v)
 	return *this;
 }
 
-inline vec3 &vec3::operator-=(const vec3 &v)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator-=(const vec3 &v)
 {
 	x -= v.x;
 	y -= v.y;
@@ -58,7 +146,8 @@ inline vec3 &vec3::operator-=(const vec3 &v)
 	return *this;
 }
 
-inline vec3 &vec3::operator*=(const vec3 &v)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator*=(const vec3 &v)
 {
 	x *= v.x;
 	y *= v.y;
@@ -66,7 +155,8 @@ inline vec3 &vec3::operator*=(const vec3 &v)
 	return *this;
 }
 
-inline vec3 &vec3::operator/=(const vec3 &v)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator/=(const vec3 &v)
 {
 	x /= v.x;
 	y /= v.y;
@@ -74,7 +164,8 @@ inline vec3 &vec3::operator/=(const vec3 &v)
 	return *this;
 }
 
-inline vec3 &vec3::operator+=(real_t c)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator+=(comp_t c)
 {
 	x += c;
 	y += c;
@@ -82,7 +173,8 @@ inline vec3 &vec3::operator+=(real_t c)
 	return *this;
 }
 
-inline vec3 &vec3::operator-=(real_t c)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator-=(comp_t c)
 {
 	x -= c;
 	y -= c;
@@ -90,7 +182,8 @@ inline vec3 &vec3::operator-=(real_t c)
 	return *this;
 }
 
-inline vec3 &vec3::operator*=(real_t c)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator*=(comp_t c)
 {
 	x *= c;
 	y *= c;
@@ -98,7 +191,8 @@ inline vec3 &vec3::operator*=(real_t c)
 	return *this;
 }
 
-inline vec3 &vec3::operator/=(real_t c)
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::operator/=(comp_t c)
 {
 	x /= c;
 	y /= c;
@@ -106,97 +200,10 @@ inline vec3 &vec3::operator/=(real_t c)
 	return *this;
 }
 
-inline vec3 &vec3::normalize()
+template<class comp_t>
+inline vec3<comp_t> &vec3<comp_t>::normalize()
 {
-	real_t invl = real_t(1) / length();
+	comp_t invl = comp_t(1) / length();
 	*this *= invl;
 	return *this;
-}
-
-inline std::istream &operator>>(std::istream &is, vec3 &v)
-{
-	is >> v.x >> v.y >> v.z;
-	return is;
-}
-
-inline std::ostream &operator<<(std::ostream &os, const vec3 &v)
-{
-	os << v.x << " " << v.y << " " << v.z;
-	return os;
-}
-
-inline vec3 operator+(const vec3 &a, const vec3 &b)
-{
-	return vec3(a.x + b.x, a.y + b.y, a.z + b.z);
-}
-
-inline vec3 operator-(const vec3 &a, const vec3 &b)
-{
-	return vec3(a.x - b.x, a.y - b.y, a.z - b.z);
-}
-
-inline vec3 operator*(const vec3 &a, const vec3 &b)
-{
-	return vec3(a.x * b.x, a.y * b.y, a.z * b.z);
-}
-
-inline vec3 operator/(const vec3 &a, const vec3 &b)
-{
-	return vec3(a.x / b.x, a.y / b.y, a.z / b.z);
-}
-
-inline vec3 operator+(const vec3 &v, real_t c)
-{
-	return vec3(v.x + c, v.y + c, v.z + c);
-}
-
-inline vec3 operator-(const vec3 &v, real_t c)
-{
-	return vec3(v.x - c, v.y - c, v.z - c);
-}
-
-inline vec3 operator*(const vec3 &v, real_t c)
-{
-	return vec3(v.x * c, v.y * c, v.z * c);
-}
-
-inline vec3 operator/(const vec3 &v, real_t c)
-{
-	return vec3(v.x / c, v.y / c, v.z / c);
-}
-
-inline vec3 operator+(real_t c, const vec3 &v)
-{
-	return v + c;
-}
-
-inline vec3 operator-(real_t c, const vec3 &v)
-{
-	return v - c;
-}
-
-inline vec3 operator*(real_t c, const vec3 &v)
-{
-	return v * c;
-}
-
-inline vec3 operator/(real_t c, const vec3 &v)
-{
-	return vec3(c / v.x, c / v.y, c / v.z);
-}
-
-inline real_t dot(const vec3 &a, const vec3 &b)
-{
-	return a.x * b.x + a.y * b.y + a.z * b.z;
-}
-
-inline vec3 cross(const vec3 &a, const vec3 &b)
-{
-	return vec3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
-}
-
-inline vec3 normalize(const vec3 &v)
-{
-	real_t invl = real_t(1) / v.length();
-	return v * invl;
 }

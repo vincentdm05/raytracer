92fcc037cc4515d84ce7b6bc123a4ace6702545c
Vincent de Marignac <vincentdm_05@yahoo.fr>
2019-04-27 11:21:44 +0300

    add copy ctor because move ctor implicitly deleted it

--- Vec3.hpp
+++ Vec3.hpp
@@ -21,6 +21,7 @@ public:
 	Vec3() { x = 0; y = 0; z = 0; }
 	Vec3(Real _x, Real _y, Real _z) { x = _x; y = _y; z = _z; }
 	Vec3(Vec3 && other) { x = std::move(other.x); y = std::move(other.y); z = std::move(other.z); }
+	Vec3(const Vec3 &other) { x = other.x; y = other.y; z = other.z; }
 
 	inline Vec3 &operator=(const Vec3 &v) { if (this != &v) { x = v.x; y = v.y; z = v.z; }; return *this; }
 

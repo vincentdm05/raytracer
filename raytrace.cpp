83c237e746e02d7bbf3a6f383b9c04c6cb655597
Vincent de Marignac <vincentdm_05@yahoo.fr>
2019-05-11 15:35:50 +0300

    add roughness scene

--- raytrace.cpp
+++ raytrace.cpp
@@ -245,6 +245,40 @@ void testSceneRandom(const Raytracer &raytracer)
 	materials.clear();
 }
 
+void testSceneRoughness(const Raytracer &raytracer)
+{
+	Vec3 cameraPosition(0.0, 1.0, 11.8);
+	Vec3 focusPosition(0, 0, -1);
+	Vec3 focusDirection = focusPosition - cameraPosition;
+	Camera camera(cameraPosition, focusDirection, Vec3(0.0, 1.0, 0.0), 5.0, Viewport(900, 110), 0.2, focusDirection.length());
+
+	Scene scene;
+	scene.setBackground(Background(Vec3(0.619, 1, 0.694), Vec3(1, 0.639, 0.619)));
+
+	Metal groundMaterial(Vec3(0.2, 0.8, 0.3), 1.0);
+	Sphere groundSphere(Vec3(0.0, -100.5, 0.0), 100.0, groundMaterial);
+	scene.add(groundSphere);
+
+	uint nSpheres = 9;
+	std::vector<Material *> materials;
+	std::vector<Sphere *> spheres;
+	for (unsigned int i = 0; i < nSpheres; i++)
+	{
+		materials.push_back(new Metal(Vec3(0.8, 0.8, 0.8), Real(i) / Real(nSpheres - 1)));
+		spheres.push_back(new Sphere(Vec3(-(nSpheres / 2.0) + Real(i) + 0.5, 0.0, -1.0), 0.5, *materials.back()));
+		scene.add(*spheres.back());
+	}
+
+	raytracer.printImage(scene, camera);
+
+	for (Material *material : materials)
+		delete material;
+	materials.clear();
+	for (Sphere *sphere : spheres)
+		delete sphere;
+	spheres.clear();
+}
+
 int main()
 {
 	Raytracer raytracer;
@@ -255,6 +289,7 @@ int main()
 	// testScenePositionableCamera(raytracer);
 	// testSceneRef(raytracer);
 	testSceneRandom(raytracer);
+	// testSceneRoughness(raytracer);
 
 	// raytracer.setOutputEnabled(false);
 	// auto start = std::chrono::system_clock::now();

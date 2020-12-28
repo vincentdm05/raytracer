d2da4ceeffae057538f85d75024bc2e579c18177
Vincent de Marignac <vincentdm_05@yahoo.fr>
2020-12-28 22:40:58 +0200

    remove unused sphere hit implementation

--- Sphere.hpp
+++ Sphere.hpp
@@ -32,26 +32,6 @@ bool Sphere::hit(const Ray &r, Real minDist, Real maxDist, HitRecord &rec) const
 	bool hit = false;
 	if (discriminant > 0.0)
 	{
-		// discriminant = sqrt(discriminant);
-		// Real temp = (-b - discriminant) / a;
-		// if (temp > minDist && temp < maxDist)
-		// {
-		// 	rec.t = temp;
-		// 	rec.point = r.to(rec.t);
-		// 	rec.normal = (rec.point - center) / radius;
-		//	rec.material = material;
-		// 	return true;
-		// }
-		// temp = (-b + discriminant) / a;
-		// if (temp > minDist && temp < maxDist)
-		// {
-		// 	rec.t = temp;
-		// 	rec.point = r.to(rec.t);
-		// 	rec.normal = (rec.point - center) / radius;
-		//	rec.material = material;
-		// 	return true;
-		// }
-
 		discriminant = sqrt(discriminant);
 		minDist = minDist * a + b;
 		maxDist = maxDist * a + b;
